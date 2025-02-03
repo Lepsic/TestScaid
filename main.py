d5372b2281862e066b6fb20df5ade060360f97fb
lepsick <Lepsick2003@yandex.ru>
2025-02-03 19:17:56 +0300

    fix.

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from databases_sync	import DatabaseSync
 
 
 async def sync_pipeline():
-	databases_sync_instance =  DatabaseSync()
+	databases_sync_instance = DatabaseSync()
+	await databases_sync_instance.fetch_data()
 	await databases_sync_instance.sync_databases()
+	print(databases_sync_instance.source_database)
 
 if __name__ == "__main__":
 	asyncio.run(sync_pipeline())
